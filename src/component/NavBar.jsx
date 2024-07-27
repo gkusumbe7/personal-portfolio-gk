69b1ce953832598d6712593e69080423209e739a
Ganesh Kusumbe <gkusumbe7@gmail.com>
2024-07-28 00:50:36 +0530

    added code

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -88,7 +88,7 @@ export default function NavBar() {
   );
 
   return (
-    <Navbar className="fixed z-50 w-full  mt-1 mx-5 px-4 py-2 lg:px-8 lg:py-4 border-blue-200 bg-gray-800">
+    <Navbar className="fixed z-50 w-full mt-1  py-2 lg:px-8 lg:py-4 border-blue-200 bg-gray-800">
       <div className="container mx-auto flex items-center justify-between">
         <Typography
           as="a"
