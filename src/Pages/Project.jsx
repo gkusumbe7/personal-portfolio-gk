4758c2ca56cb3d4d1c63a6c1080756b4c338edc5
Ganesh Kusumbe <gkusumbe7@gmail.com>
2024-08-04 14:42:51 +0530

    added code

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -8,9 +8,8 @@ import {
 } from "@material-tailwind/react";
 import NavBar from "../component/NavBar";
 export default function Project() {
-  // import imggk from
-  const projectData = [
-    {
+const projectData = [
+      {
       id: 1,
       name: "Item 1",
       title: "Weather App",
@@ -29,24 +28,26 @@ export default function Project() {
       liveLink:"https://geekfood-drab.vercel.app/"
     },
     {
-      id: 1,
-      name: "Item 1",
-      title: "Weather App",
-      description:"Get real-time updates on temperature, humidity, wind speed, and more",
-      image: "../assets/weatherApp.PNG",
-      codelink: "https://github.com/gkusumbe7/weather-app",
-      liveLink:"https://weather-app-eight-theta-13.vercel.app/",
-      techicalLanguages:"React js , Tailwind Css"
+      id: 3,
+      name: "TWUBRIC APP",
+      title: "TWUBRIC APP",
+      description:"Rate and review tweets and Twitter accounts effortlessly. Discover trending content and engage with the community through our intuitive rating system.",
+      image: "https://raw.githubusercontent.com/gkusumbe7/personal-portfolio-gk/main/src/assets/TA.PNG",
+      codelink: "https://github.com/gkusumbe7/Twubric-App",
+      liveLink:"https://twubric-app.vercel.app/"
     },
     {
-      id: 1,
-      name: "Item 1",
-      title: "Weather App",
-      description:"Get real-time updates on temperature, humidity, wind speed, and more",
-      image: "../assets/weatherApp.PNG",
-      codelink: "https://github.com/gkusumbe7/weather-app",
-      liveLink:"https://weather-app-eight-theta-13.vercel.app/"
+      id: 4,
+      name: "Password Generator App",
+      title: "Password Generator App",
+      description:"A food recipe app offers a variety of recipes with searchable ingredients, cooking instructions, and meal planning tools, helping users easily find and prepare meals.",
+      image: "https://raw.githubusercontent.com/gkusumbe7/personal-portfolio-gk/main/src/assets/PG.PNG",
+      codelink: "https://github.com/gkusumbe7/Password-Generator",
+      liveLink:"https://password-generator-lac-six.vercel.app/"
     },
+    
+    
+    
   ];
 
   return (
@@ -73,84 +74,12 @@ export default function Project() {
                 </Typography>
               </CardBody>
               <CardFooter className="pt-1 pb-2 flex justify-around">
-                <Button><a href={data.codelink}>CODE</a></Button>
-                <Button><a href={data.liveLink}>LIVE</a></Button>
+              <a href={data.codelink}><Button>CODE</Button></a>
+              <a href={data.liveLink}><Button>LIVE</Button></a>
               </CardFooter>
             </Card>
           </main>
         ))}
-
-        {/* <main className="flex flex-col-reverse md:flex-row gap-10  md:gap-10 md:p-5 my-1 lg:my-auto">
-          <Card className="pb-0">
-            <CardHeader color="blue-gray" className="">
-              <img
-                src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-                alt="card-image"
-                className="rounded-xl "
-              />
-            </CardHeader>
-            <CardBody className="p-3 px-4">
-              <Typography variant="h4" color="blue-gray">
-                UI/UX Review Check
-              </Typography>
-              <Typography>
-                The place is close to Barceloneta Beach and bus stop just 2 min
-                by walk and near to &quot;Naviglio&quot; where you can enjoy the
-                main night life in Barcelona.
-              </Typography>
-            </CardBody>
-            <CardFooter className="pt-1 pb-2 flex justify-around">
-              <Button>Read More</Button>
-              <Button>Read More</Button>
-            </CardFooter>
-          </Card>
-          <Card className="pb-0">
-            <CardHeader color="blue-gray" className="">
-              <img
-                src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-                alt="card-image"
-                className="rounded-xl "
-              />
-            </CardHeader>
-            <CardBody className="p-3 px-4">
-              <Typography variant="h4" color="blue-gray">
-                UI/UX Review Check
-              </Typography>
-              <Typography>
-                The place is close to Barceloneta Beach and bus stop just 2 min
-                by walk and near to &quot;Naviglio&quot; where you can enjoy the
-                main night life in Barcelona.
-              </Typography>
-            </CardBody>
-            <CardFooter className="pt-1 pb-2 flex justify-around">
-              <Button>Read More</Button>
-              <Button>Read More</Button>
-            </CardFooter>
-          </Card>
-          <Card className="pb-0">
-            <CardHeader color="blue-gray" className="p-0">
-              <img
-                src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-                alt="card-image"
-                className="rounded-xl"
-              />
-            </CardHeader>
-            <CardBody className="p-3 px-4">
-              <Typography variant="h4" color="blue-gray">
-                UI/UX Review Check
-              </Typography>
-              <Typography>
-                The place is close to Barceloneta Beach and bus stop just 2 min
-                by walk and near to &quot;Naviglio&quot; where you can enjoy the
-                main night life in Barcelona.
-              </Typography>
-            </CardBody>
-            <CardFooter className="pt-1 pb-2 flex justify-around">
-              <Button>Read More</Button>
-              <Button>Read More</Button>
-            </CardFooter>
-          </Card>
-        </main> */}
       </main>
     </>
   );
