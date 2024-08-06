802ded719ba7544a5abff20d608afa60b91ac214
Ganesh Kusumbe <gkusumbe7@gmail.com>
2024-08-06 23:50:56 +0530

    added code

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -62,7 +62,7 @@ const projectData = [
                   // src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
                   src={data.image}
                   alt="card-image"
-                  className="rounded-xl border border-teal-400 "
+                  className="rounded-xl border border-teal-500 "
                 />
               </CardHeader>
               <CardBody className="p-3 px-4">
